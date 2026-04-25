fix bug in old genqo sigsag.fidelity causing precision issue

test/python/test_compare_with_python.py
@@ -253,8 +253,7 @@ def test_sigsag__fidelity(sigsag_py: gqpy.SIGSAG_BS, sigsag_jl: gqjl.SIGSAG, sig
         sigsag_jl.set(**params)
         fidelity_jl = sigsag_jl.fidelity()
 
-        # Relax tolerance here because tests were failing due to a small numerical difference, even when implementation is correct
-        assert np.isclose(fidelity_py, fidelity_jl, atol=1e-2), error_with_params(params)
+        assert np.isclose(fidelity_py, fidelity_jl, atol=tol), error_with_params(params)
         precision_table.append({
             "function": "sigsag.fidelity",
             "params": params,