43cdc86296a202f370c059710d86a4eea925dcb9
Jacob Gunnell <jacob.r.gunnell@gmail.com>
2026-07-09 18:16:26 -0600

    reorganize tutorial files for zalm and 3-source pbp

--- docs/tutorial/pbp3.jl
+++ docs/tutorial/pbp3.jl
@@ -4,26 +4,28 @@ using Gabs
 using Plots
 
 
-function pbp(μ::Float64, ηᵗ::Float64, ηᵇ::Float64, ηᵍ::Float64; proj::HybridProjector)
+# 3-source PBP model
+
+function pbp3(μ::Float64, ηᵗ::Float64, ηᵇ::Float64, ηᵍ::Float64; proj::HybridProjector)
     basis2 = QuadBlockBasis(2)
     basis4 = QuadBlockBasis(4)
     sagnac = eprstate(basis4, asinh(√μ), 0.)
     apply!(sagnac, modeswap(basis2), [1,3])
     st = sagnac ⊗ sagnac ⊗ sagnac
 
-    apply!(st, beamsplitter(basis4, 0.5), [3,5,4,6])
-    apply!(st, greenmachine(basis4, 4), [7,8,9,10])
+    apply!(st, beamsplitter(basis4, 0.5), [3,5, 4,6])
+    apply!(st, greenmachine(basis4, 4), [7,8, 9,10])
 
     η = [ηᵗ,ηᵗ,ηᵇ,ηᵇ,ηᵇ,ηᵇ,ηᵍ,ηᵍ,ηᵍ,ηᵍ,ηᵗ,ηᵗ]
     project(st, proj, [:,:,1,0,0,1,1,0,0,1,:,:]; η=η)
 end
 
-function plot_pbp_probability()
+## Probability of generation
+function plot_pbp3_probability()
     proj = HybridProjector(12)
-    # μ = logrange(1e-4, 1e2, 100)
     μ = range(1e-3, 1, 100)
     η = [1., 0.8, 0.5, 0.25]
-    states = pbp.(μ, 1., η', η'; proj=proj)
+    states = pbp3.(μ, 1., η', η'; proj=proj)
     Pgen = tr.(states)
 
     plot(μ, Pgen[:,1], label="\\eta = 1.0", xlabel="Mean Photon Number Per Mode", ylabel="Probability of Success", legend=:bottomright, color=1)
@@ -31,18 +33,17 @@ function plot_pbp_probability()
     plot!(μ, Pgen[:,3], label="\\eta = 0.5", color=3)
     plot!(μ, Pgen[:,4], label="\\eta = 0.25", color=4)
     # True 3-pair heralds + 4-pair false heralds (both photons of an outer-source double pair detected in the same BSM)
-    plot!(μ, μ.^3 ./ (μ.+1).^9 ./ 8 .+ μ.^4 ./ (μ.+1).^10 ./ 16, label="\\eta = 1.0 analytical", linestyle=:dot, color=1)
+    plot!(μ, μ.^3 ./ (μ.+1).^9 ./ 8 .+ μ.^4 ./ (μ.+1).^10 ./ 16, label="\\eta = 1.0 analytical", linestyle=:dash, color=:blue)
 end
-@time plot_pbp_probability()
+@time plot_pbp3_probability()
 
-function plot_pbp_fidelity()
+## Fidelity
+function plot_pbp3_fidelity()
     proj = HybridProjector(12)
-    # μ = logrange(1e-4, 1e3, 20)   
     μ = range(1e-6, 0.1, 20)
     η = [1., 0.8, 0.5, 0.25]
-    states = pbp.(μ, 1., η', η'; proj=proj)
+    states = pbp3.(μ, 1., η', η'; proj=proj)
     ψ⁻ = (clicks([1,0,0,1]) + clicks([0,1,1,0])) / √2
-    # F = @. real(dot([ψ⁻'], states, [ψ⁻])) / tr(states)
     F = similar(states, Float64)
     Threads.@threads for I in CartesianIndices(states)
         F[I] = real(dot(ψ⁻', states[I], ψ⁻)) / tr(states[I])
@@ -53,4 +54,4 @@ function plot_pbp_fidelity()
     plot!(μ, F[:,3], label="\\eta = 0.5")
     plot!(μ, F[:,4], label="\\eta = 0.25")
 end
-@time plot_pbp_fidelity()
+@time plot_pbp3_fidelity()
---
+++ docs/tutorial/zalm2.jl
@@ -0,0 +1,55 @@
+using Genqo
+using Gabs
+
+using Plots
+
+
+# ZALM model
+
+function zalm2(μ::Float64, ηᵗ::Float64, ηᵈ::Float64; proj::HybridProjector, metric::Symbol=:none)
+    basis4 = QuadBlockBasis(4)
+    basis8 = QuadBlockBasis(8)
+    st = eprstate(basis8, asinh(√μ), 0.)
+    ms = modeswap(basis4)
+    bs = beamsplitter(basis4, 0.5)
+
+    apply!(st, ms, [2,4, 5,7])
+    apply!(st, bs, [3,5, 4,6])
+
+    η = [ηᵗ,ηᵗ,ηᵈ,ηᵈ,ηᵈ,ηᵈ,ηᵗ,ηᵗ]
+    project(st, proj, [:,:,1,1,0,0,:,:]; η=η)
+end
+
+## Probability of generation
+function plot_zalm2_probability()
+    proj = HybridProjector(8)
+    μ = logrange(1e-4, 10, 100)
+    η = 10 .^ -([0, 3, 6, 9]/10)
+    states = zalm2.(μ, η', η'; proj=proj)
+
+    Pgen = tr.(states)
+    Pgen_ground = zalm.probability_success.(μ, η', 1, η', 0)
+
+    plot(μ, Pgen_ground, label="Genqo v1 (ground truth)", xscale=:log10, yscale=:log10, xlabel="Mean Photon Number Per Mode", ylabel="Probability of Success", legend=:bottomright, color=[1 2 3 4])
+    plot!(μ, Pgen, label="Genqo v2", linestyle=:dash, color=[:blue :orange :green :red])
+    plot!(μ, μ.^2 ./ (μ.+1).^6, label="Analytical", linestyle=:dot, color=:black)
+end
+@time plot_zalm2_probability()
+
+## Fidelity
+function plot_zalm2_fidelity()
+    proj = HybridProjector(8)
+    μ = logrange(1e-4, 10, 100)
+    η = 10 .^ -([0, 3, 6, 9]/10)
+    states = zalm2.(μ, η', η'; proj=proj)
+    ψ⁺ = (clicks([1,0,0,1]) + clicks([0,1,1,0])) / √2
+    F = similar(states, Float64)
+    Threads.@threads for I in CartesianIndices(states)
+        F[I] = real(dot(ψ⁺', states[I], ψ⁺)) / tr(states[I])
+    end
+    F_ground = zalm.fidelity.(μ, η', 1, η')
+
+    plot(μ, F_ground, label="Genqo v1 (ground truth)", xscale=:log10, xlabel="Mean Photon Number Per Mode", ylabel="Fidelity", legend=:topleft, color=[1 2 3 4])
+    plot!(μ, F, label="Genqo v2", linestyle=:dash, color=[1 2 3 4])
+end
+@time plot_zalm2_fidelity()
