94dc62a18e89f591bb723c4f7201eb93980cd035
Jacob Gunnell <jacob.r.gunnell@gmail.com>
2026-07-15 10:40:30 -0600

    rename BSM loss to eta_b for consistency

--- docs/tutorial/zalm2.jl
+++ docs/tutorial/zalm2.jl
@@ -6,12 +6,12 @@ using Plots
 
 # ZALM model
 
-function zalm2(μ::Float64, ηᵗ::Float64, ηᵈ::Float64; engine::HybridProjectionEngine)
+function zalm2(μ::Float64, ηᵗ::Float64, ηᵇ::Float64; engine::HybridProjectionEngine)
     st = eprstate(QuadBlockBasis(8), asinh(√μ), 0.)
     apply!(st, modeswap(QuadBlockBasis(4)), [2,4, 5,7])
     apply!(st, beamsplitter(QuadBlockBasis(4), 0.5), [3,5, 4,6])
 
-    η = [ηᵗ,ηᵗ,ηᵈ,ηᵈ,ηᵈ,ηᵈ,ηᵗ,ηᵗ]
+    η = [ηᵗ,ηᵗ,ηᵇ,ηᵇ,ηᵇ,ηᵇ,ηᵗ,ηᵗ]
     Π = projector([:,:,1,1,0,0,:,:])
     project(st, Π; engine, η=η)
 end
@@ -21,10 +21,10 @@ function plot_zalm2_probability()
     engine = HybridProjectionEngine(8)
     μ = logrange(1e-4, 10, 100)
     η = 10 .^ -([0, 3, 6, 9]/10)
-    states = zalm2.(μ, η', η'; engine=engine)
+    states = zalm2.(μ, 1., η'; engine=engine)
 
     Pgen = tr.(states)
-    Pgen_ground = zalm.probability_success.(μ, η', 1, η', 0)
+    Pgen_ground = zalm.probability_success.(μ, 1., 1., η', 0)
 
     plot(μ, Pgen_ground, label="Genqo v1 (ground truth)", xscale=:log10, yscale=:log10, xlabel="Mean Photon Number Per Mode", ylabel="Probability of Generation", legend=:bottomright, color=[1 2 3 4])
     plot!(μ, Pgen, label="Genqo v2", linestyle=:dash, color=[:blue :orange :green :red])
@@ -72,8 +72,8 @@ function plot_zalm2_Bell_state_fraction()
         real(dot(ψ⁺', st, ψ⁺) + dot(ψ⁻', st, ψ⁻) + dot(ϕ⁺', st, ϕ⁺) + dot(ϕ⁻', st, ϕ⁻))
     end
 
-    Pload = zalm2_Pload.(μ, 0.01, 0.9; engine=engine)
-    PBell = zalm2_PBell.(μ, 0.01, 0.9; engine=engine)
+    Pload = zalm2_Pload.(μ, 1., 0.9; engine=engine)
+    PBell = zalm2_PBell.(μ, 1., 0.9; engine=engine)
     B = PBell ./ Pload
     
     plot(μ, B, label="ZALM", xlabel="Mean Photon Number Per Mode", ylabel="Bell-state Fraction", legend=:bottomright, color=1)
@@ -85,15 +85,15 @@ function plot_zalm2_fidelity()
     engine = HybridProjectionEngine(8)
     μ = logrange(1e-4, 10, 100)
     η = 10 .^ -([0, 3, 6, 9]/10)
-    states = zalm2.(μ, η', η'; engine=engine)
+    states = zalm2.(μ, 1., η'; engine=engine)
     ψ⁺ = (clicks([1,0,0,1]) + clicks([0,1,1,0])) / √2
     F = similar(states, Float64)
     Threads.@threads for I in CartesianIndices(states)
         F[I] = real(dot(ψ⁺', states[I], ψ⁺)) / tr(states[I])
     end
-    F_ground = zalm.fidelity.(μ, η', 1, η')
+    F_ground = zalm.fidelity.(μ, 1., 1., η')
 
-    plot(μ, F_ground, label="Genqo v1 (ground truth)", xscale=:log10, xlabel="Mean Photon Number Per Mode", ylabel="Fidelity", legend=:topleft, color=[1 2 3 4])
+    plot(μ, F_ground, label="Genqo v1 (ground truth)", ylim=[0,1], xscale=:log10, xlabel="Mean Photon Number Per Mode", ylabel="Fidelity", legend=:topleft, color=[1 2 3 4])
     plot!(μ, F, label="Genqo v2", linestyle=:dash, color=[1 2 3 4])
 end
 @time plot_zalm2_fidelity()
