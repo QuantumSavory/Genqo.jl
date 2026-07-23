dda323fbd791044466f431c1db0bb5ff433b79f9
Jorge Vasquez <jomavaca123@gmail.com>
2026-07-23 14:35:58 -0600

    Fidelity, bell purity, initial guess for zalm and chahine root at RE.

--- docs/tutorial/rate_dist_entanglement.jl
+++ docs/tutorial/rate_dist_entanglement.jl
@@ -36,6 +36,22 @@ function E_H(p, N_M::Integer)
 end
 
 #Zalm
+function zalm_Pbell_numerical(μ::Float64, ηR::Float64, ηT::Float64; engine::HybridProjectionEngine)
+    st = eprstate(QuadBlockBasis(8), asinh(√μ), 0.)
+    apply!(st, modeswap(QuadBlockBasis(4)), [2,4,5,7])
+    apply!(st, beamsplitter(QuadBlockBasis(4), 0.5), [3,5,4,6])
+
+    η = [ηR,ηR,ηT,ηT,ηT,ηT,ηR,ηR]
+    Π = projector([:,:,1,1,0,0,:,:])
+    st = project(st, Π; engine, η=η)
+
+    ψ⁺ = (clicks([1,0,0,1]) + clicks([0,1,1,0])) / √2
+    ψ⁻ = (clicks([1,0,0,1]) - clicks([0,1,1,0])) / √2
+    ϕ⁺ = (clicks([1,0,1,0]) + clicks([0,1,0,1])) / √2
+    ϕ⁻ = (clicks([1,0,1,0]) - clicks([0,1,0,1])) / √2
+
+    real(dot(ψ⁺', st, ψ⁺) + dot(ψ⁻', st, ψ⁻) + dot(ϕ⁺', st, ϕ⁺) + dot(ϕ⁻', st, ϕ⁻))
+end
 function zalm_Pload_numerical(μ::Float64, ηR::Float64, ηT::Float64; engine::HybridProjectionEngine)
     st = eprstate(QuadBlockBasis(8), asinh(√μ), 0.)
     apply!(st, modeswap(QuadBlockBasis(4)), [2,4, 5,7])
@@ -52,6 +68,12 @@ function zalm_Pload_numerical(μ::Float64, ηR::Float64, ηT::Float64; engine::H
     st_S0 = project(st, Π_S0; engine, η=η)
 
     tr(st_S) - tr(st_A0) - tr(st_B0) + tr(st_S0)
+end
+
+function zalm_bell_fraction_numerical(μ::Float64, ηR::Float64, ηT::Float64; engine::HybridProjectionEngine)
+    Pload = zalm_Pload_numerical.(μ, ηR, ηT; engine=engine)
+    PBell = zalm_Pbell_numerical.(μ, ηR, ηT; engine=engine)
+    PBell ./ Pload
 end
 
 function zalm_rate_dist_entanglement_numerical(μ::Float64, ηR::Float64, ηT::Float64, Rp::Float64, N_I::Integer, N_M; engine::HybridProjectionEngine)
@@ -142,6 +164,45 @@ function zalm_rate_dist_entanglement_symbolic(μ::Float64, ηR::Float64, ηT::Fl
     return Rp * EH * F * Pbell
 end  
 #Chahine
+function chahine_Pbell_numerical(μ::Float64, ηR::Float64, ηT::Float64; engine::HybridProjectionEngine)
+    st = eprstate(QuadBlockBasis(4), asinh(√μ), 0.) ⊗ vacuumstate(QuadBlockBasis(2))
+    apply!(st, modeswap(QuadBlockBasis(2)), [2,4])
+    apply!(st, beamsplitter(QuadBlockBasis(4), 0.5), [3,5, 4,6])
+
+    η = [ηT,ηT,ηR,ηR,ηR,ηR]
+    Π_S = projector([1,1,:,:,:,:])
+
+    st = project(st, Π_S; engine=engine, η=η)
+    ψ⁺ = (clicks([1,0,0,1]) + clicks([0,1,1,0])) / √2
+    ψ⁻ = (clicks([1,0,0,1]) - clicks([0,1,1,0])) / √2
+    ϕ⁺ = (clicks([1,0,1,0]) + clicks([0,1,0,1])) / √2
+    ϕ⁻ = (clicks([1,0,1,0]) - clicks([0,1,0,1])) / √2
+
+    real(dot(ψ⁺', st, ψ⁺) + dot(ψ⁻', st, ψ⁻) + dot(ϕ⁺', st, ϕ⁺) + dot(ϕ⁻', st, ϕ⁻))
+end
+function chahine_Pload_numerical(μ::Float64, ηR::Float64, ηT::Float64; engine::HybridProjectionEngine)
+    st = eprstate(QuadBlockBasis(4), asinh(√μ), 0.) ⊗ vacuumstate(QuadBlockBasis(2))
+    apply!(st, modeswap(QuadBlockBasis(2)), [2,4])
+    apply!(st, beamsplitter(QuadBlockBasis(4), 0.5), [3,5, 4,6])
+
+    η = [ηT,ηT,ηR,ηR,ηR,ηR]
+    Π_S = projector([1,1,:,:,:,:])
+    Π_A0 = projector([1,1,0,0,:,:])
+    Π_B0 = projector([1,1,:,:,0,0])
+    Π_S0 = projector([1,1,0,0,0,0])
+
+    st_S = project(st, Π_S; engine=engine, η=η)
+    st_A0 = project(st, Π_A0; engine=engine, η=η)
+    st_B0 = project(st, Π_B0; engine=engine, η=η)
+    st_S0 = project(st, Π_S0; engine=engine, η=η)
+
+    tr(st_S) - tr(st_A0) - tr(st_B0) + tr(st_S0)
+end
+function chahine_bell_fraction_numerical(μ::Float64, ηR::Float64, ηT::Float64; engine::HybridProjectionEngine)
+    Pbell = chahine_Pbell_numerical.(μ, ηR, ηT; engine=engine)
+    Pload = chahine_Pload_numerical.(μ, ηR, ηT; engine=engine)
+    Pbell / Pload
+end
 function chahine_rate_dist_entanglement_numerical(μ::Float64, ηR::Float64, ηT::Float64, Rp::Float64, N_I::Integer, N_M; engine::HybridProjectionEngine)
      st = eprstate(QuadBlockBasis(4), asinh(√μ), 0.) ⊗ vacuumstate(QuadBlockBasis(2))
     apply!(st, modeswap(QuadBlockBasis(2)), [2,4])
@@ -171,7 +232,7 @@ function chahine_Pbell_symbolic(μ::Float64, ηR::Float64, ηT::Float64)
     Ñs = 2*N′s/(N′s + 1)
     
     Pr_Bell = N′s^2 * (
-        3*(1 - N′s)^2 / 2
+        3*(1 - N′s)^2 /
         - (3*ηR*N′s*(1 - 2*N′s)*(1 - N′s)) / Ns
         + (ηR^2*N′s^2*((1 - 2*N′s)^2 + 2*(1 - 3*N′s)*(1 - N′s))) / (2*Ns^2)
     )
@@ -228,23 +289,23 @@ function spdc_Pbell_symbolic(μ::Float64, ηR::Float64)::Float64
     Pr_Bell = (ηR^2 * μ * (3*G - 1 + ηR*(ηR - 2)*μ) + 3*(ηR*(ηR - 1)*μ)^2) / ((ηR*(ηR - 2)*μ - 1)^4)
     return Pr_Bell
 end
-function spdc_rate_dist_entanglement_symbolic(μ::Float64, ηR::Float64, Rp::Float64, N_M::Int)
-    st = eprstate(QuadBlockBasis(4), asinh(√μ), 0.)
-    apply!(st, modeswap(QuadBlockBasis(2)), [2,4])
-    η = [ηR,ηR,ηR,ηR]
-    Π = projector([:,:,:,:])
-    st = project(st, Π; engine, η=η)
+# function spdc_rate_dist_entanglement_symbolic(μ::Float64, ηR::Float64, Rp::Float64, N_M::Int)
+#     st = eprstate(QuadBlockBasis(4), asinh(√μ), 0.)
+#     apply!(st, modeswap(QuadBlockBasis(2)), [2,4])
+#     η = [ηR,ηR,ηR,ηR]
+#     Π = projector([:,:,:,:])
+#     st = project(st, Π; engine, η=η)
 
-    ψ⁺ = (clicks([1,0,0,1]) + clicks([0,1,1,0])) / √2
-    ψ⁻ = (clicks([1,0,0,1]) - clicks([0,1,1,0])) / √2
-    ϕ⁺ = (clicks([1,0,1,0]) + clicks([0,1,0,1])) / √2
-    ϕ⁻ = (clicks([1,0,1,0]) - clicks([0,1,0,1])) / √2
+#     ψ⁺ = (clicks([1,0,0,1]) + clicks([0,1,1,0])) / √2
+#     ψ⁻ = (clicks([1,0,0,1]) - clicks([0,1,1,0])) / √2
+#     ϕ⁺ = (clicks([1,0,1,0]) + clicks([0,1,0,1])) / √2
+#     ϕ⁻ = (clicks([1,0,1,0]) - clicks([0,1,0,1])) / √2
 
-    PBell = real(dot(ψ⁺', st, ψ⁺) + dot(ψ⁻', st, ψ⁻) + dot(ϕ⁺', st, ϕ⁺) + dot(ϕ⁻', st, ϕ⁻))
-    F = real(dot(ψ⁺', st, ψ⁺))/ real(dot(ψ⁺', st, ψ⁺) + dot(ψ⁻', st, ψ⁻) + dot(ϕ⁺', st, ϕ⁺) + dot(ϕ⁻', st, ϕ⁻))
+#     PBell = real(dot(ψ⁺', st, ψ⁺) + dot(ψ⁻', st, ψ⁻) + dot(ϕ⁺', st, ϕ⁺) + dot(ϕ⁻', st, ϕ⁻))
+#     F = real(dot(ψ⁺', st, ψ⁺))/ real(dot(ψ⁺', st, ψ⁺) + dot(ψ⁻', st, ψ⁻) + dot(ϕ⁺', st, ϕ⁺) + dot(ϕ⁻', st, ϕ⁻))
 
-    return Rp * N_M * PBell * F
-end
+#     return Rp * N_M * PBell * F
+# end
 
 # pbp
 
@@ -298,7 +359,7 @@ function plot_rate_dist_entanglement_with_Nm_3D()
     chahine_rates = [chahine_rate_dist_entanglement_numerical(μ, ηR, ηT, Rp, N_I, N_M; engine=chahine_engine)
                      for μ in μ_vals, ηT in ηT_vals]
     # spdc_rates = [spdc_rate_dist_entanglement_numerical(μ, ηR, Rp, N_M; engine=spdc_engine) for μ in μ_vals, ηT in ηT_vals]
-    spcd_rates_symbolic = [spdc_rate_dist_entanglement_symbolic(μ, ηR, Rp, N_M) for μ in μ_vals, ηT in ηT_vals]
+    #spcd_rates_symbolic = [spdc_rate_dist_entanglement_symbolic(μ, ηR, Rp, N_M) for μ in μ_vals, ηT in ηT_vals]
 
     #pbp4_rates = [pbp4_rate_dist_entanglement(μ, ηR, ηT, Rp, N_I, N_M; engine=pbp4_engine) for μ in μ_vals, ηT in ηT_vals]
     zmax = maximum(filter(!isnan, vcat(vec(zalm_rates), vec(chahine_rates))))
@@ -323,8 +384,8 @@ function plot_rate_dist_entanglement_with_Nm_3D()
         (name = "ZALM",    data = zalm_rates,    color = RGBAf(0.18, 0.545, 0.341, 0.8)),
         (name = "Chahine", data = chahine_rates, color = RGBAf(1.0,  0.271, 0.0,   0.8)),
        #(name = "PBP4", data  = pbp4_rates, color = RGBAf(0.85, 0.74,  0.0,   0.4) )
-        # (name = "SPDC",    data = spdc_rates,    color = RGBAf(0.85, 0.74,  0.0,   0.4)),
-        (name = "SPDC symbolic", data = spcd_rates_symbolic,  color = RGBAf(1.0,  0.271, 0.0,   0.8))
+        #(name = "SPDC",    data = spdc_rates,    color = RGBAf(0.85, 0.74,  0.0,   0.4)),
+       #(name = "SPDC symbolic", data = spcd_rates_symbolic,  color = RGBAf(1.0,  0.271, 0.0,   0.8))
     ]
 
     for s in surfaces
@@ -352,23 +413,32 @@ function plot_rate_dist_entanglement_with_Nm_3D()
     Rp = 10e9
     N_I = 10
     N_M = 1
-    μ_vals = logrange(1e-4, 2, 100)
+    μ_vals = logrange(1e-4, 10, 100)
 
     zalm_rates = [zalm_rate_dist_entanglement_numerical(μ, ηR, ηT, Rp, N_I, N_M; engine=zalm_engine)
                   for μ in μ_vals, ηT in ηT_vals]
     chahine_rates = [chahine_rate_dist_entanglement_numerical(μ, ηR, ηT, Rp, N_I, N_M; engine=chahine_engine)
                      for μ in μ_vals, ηT in ηT_vals]
     # spdc_rates = [spdc_rate_dist_entanglement_numerical(μ, ηR, Rp, N_M; engine=spdc_engine) for μ in μ_vals, ηT in ηT_vals]
-    spcd_rates_symbolic = [spdc_rate_dist_entanglement_symbolic(μ, ηR, Rp, N_M) for μ in μ_vals, ηT in ηT_vals]
+    #spcd_rates_symbolic = [spdc_rate_dist_entanglement_symbolic(μ, ηR, Rp, N_M) for μ in μ_vals, ηT in ηT_vals]
 
     #pbp4_rates = [pbp4_rate_dist_entanglement(μ, ηR, ηT, Rp, N_I, N_M; engine=pbp4_engine) for μ in μ_vals, ηT in ηT_vals]
     zmax = maximum(filter(!isnan, vcat(vec(zalm_rates), vec(chahine_rates))))
+        
+    function source_roots(ηT)
+        1 / ηT
+    end
+    z_vals = logrange(1e-4, zmax, 50)  
+
+    μ_curtain_matrix  = [source_roots(ηT) for _ in z_vals, ηT in ηT_vals]
+    ηT_curtain_matrix = [ηT               for _ in z_vals, ηT in ηT_vals]
+    z_curtain_matrix  = [z                for z  in z_vals, _  in ηT_vals]
 
     fig = Figure(size = (1000, 700))
 
     ax = Axis3(fig[1, 1];
         xlabel = "Mean photon number μ",
-        ylabel = "Transmission loss ηT",
+        ylabel = "Source loss ηT",
         zlabel = "Rate (bits/s)",
         title  = "Rate of distributed entanglement vs μ and ηT (NI = $(N_I), NM = $(N_M))",
         azimuth   = 35 * π/180,
@@ -385,13 +455,17 @@ function plot_rate_dist_entanglement_with_Nm_3D()
         (name = "Chahine", data = chahine_rates, color = RGBAf(1.0,  0.271, 0.0,   0.8)),
        #(name = "PBP4", data  = pbp4_rates, color = RGBAf(0.85, 0.74,  0.0,   0.4) )
         # (name = "SPDC",    data = spdc_rates,    color = RGBAf(0.85, 0.74,  0.0,   0.4)),
-        (name = "SPDC symbolic", data = spcd_rates_symbolic,  color = RGBAf(1.0,  0.271, 0.0,   0.8))
+        #(name = "SPDC symbolic", data = spcd_rates_symbolic,  color = RGBAf(1.0,  0.271, 0.0,   0.8))
     ]
 
     for s in surfaces
         surface!(ax, μ_vals, ηT_vals, s.data;
             color = fill(s.color, size(s.data)), shading = NoShading)
     end
+        
+    curtain_color = RGBAf(0.3, 0.5, 0.9, 0.5)
+    surface!(ax, μ_curtain_matrix, ηT_curtain_matrix, z_curtain_matrix;
+        color = fill(curtain_color, size(z_curtain_matrix)), shading = NoShading)
 
     Legend(fig[2, 1],
         [PolyElement(color = s.color) for s in surfaces],
@@ -404,7 +478,7 @@ end
 
 
 function plot_fidelity_with_Nm_3D()
-    zalm_engine    = HybridProjectionEngine(8)
+    zalm_engine = HybridProjectionEngine(8)
     chahine_engine = HybridProjectionEngine(6)
     spdc_engine = HybridProjectionEngine(4)
     pbp4_engine = HybridProjectionEngine(16)
@@ -414,17 +488,26 @@ function plot_fidelity_with_Nm_3D()
     Rp = 10e9
     N_I = 10
     N_M = 1
-    μ_vals = logrange(1e-4, 2, 100)
+    μ_vals = logrange(1e-4, 10, 100)
 
-    zalm_rates = [zalm_bell_fidelity_numerical(μ, ηR, ηT, Rp, N_I, N_M; engine=zalm_engine)
+    zalm_rates = [zalm_bell_fidelity_numerical(μ, ηR, ηT; engine=zalm_engine)
                   for μ in μ_vals, ηT in ηT_vals]
-    chahine_rates = [chahine_bell_fidelity_numerical(μ, ηR, ηT, Rp, N_I, N_M; engine=chahine_engine)
+    chahine_rates = [chahine_bell_fidelity_numerical(μ, ηR, ηT; engine=chahine_engine)
                      for μ in μ_vals, ηT in ηT_vals]
     # spdc_rates = [spdc_rate_dist_entanglement_numerical(μ, ηR, Rp, N_M; engine=spdc_engine) for μ in μ_vals, ηT in ηT_vals]
     #spcd_rates_symbolic = [spdc_rate_dist_entanglement_symbolic(μ, ηR, Rp, N_M) for μ in μ_vals, ηT in ηT_vals]
 
     #pbp4_rates = [pbp4_rate_dist_entanglement(μ, ηR, ηT, Rp, N_I, N_M; engine=pbp4_engine) for μ in μ_vals, ηT in ηT_vals]
     zmax = maximum(filter(!isnan, vcat(vec(zalm_rates), vec(chahine_rates))))
+              
+    function source_roots(ηT)
+        1 / ηT
+    end
+    z_vals = logrange(1e-4, zmax, 50)  
+
+    μ_curtain_matrix  = [source_roots(ηT) for _ in z_vals, ηT in ηT_vals]
+    ηT_curtain_matrix = [ηT               for _ in z_vals, ηT in ηT_vals]
+    z_curtain_matrix  = [z                for z  in z_vals, _  in ηT_vals]
 
     fig = Figure(size = (1000, 700))
 
@@ -447,13 +530,17 @@ function plot_fidelity_with_Nm_3D()
         (name = "Chahine", data = chahine_rates, color = RGBAf(1.0,  0.271, 0.0,   0.8)),
        #(name = "PBP4", data  = pbp4_rates, color = RGBAf(0.85, 0.74,  0.0,   0.4) )
         # (name = "SPDC",    data = spdc_rates,    color = RGBAf(0.85, 0.74,  0.0,   0.4)),
-        (name = "SPDC symbolic", data = spcd_rates_symbolic,  color = RGBAf(1.0,  0.271, 0.0,   0.8))
+        #(name = "SPDC symbolic", data = spcd_rates_symbolic,  color = RGBAf(1.0,  0.271, 0.0,   0.8))
     ]
 
     for s in surfaces
         surface!(ax, μ_vals, ηT_vals, s.data;
             color = fill(s.color, size(s.data)), shading = NoShading)
     end
+
+    curtain_color = RGBAf(0.3, 0.5, 0.9, 0.5)
+    surface!(ax, μ_curtain_matrix, ηT_curtain_matrix, z_curtain_matrix;
+        color = fill(curtain_color, size(z_curtain_matrix)), shading = NoShading)
 
     Legend(fig[2, 1],
         [PolyElement(color = s.color) for s in surfaces],
@@ -467,25 +554,34 @@ end
 function plot_bell_fraction_with_Nm_3D()
     zalm_engine    = HybridProjectionEngine(8)
     chahine_engine = HybridProjectionEngine(6)
-    spdc_engine = HybridProjectionEngine(4)
-    pbp4_engine = HybridProjectionEngine(16)
+    # spdc_engine = HybridProjectionEngine(4)
+    # pbp4_engine = HybridProjectionEngine(16)
 
     ηR = 0.01
     ηT_vals = logrange(0.7, 1, 100)
     Rp = 10e9
     N_I = 10
     N_M = 1
-    μ_vals = logrange(1e-4, 2, 100)
+    μ_vals = logrange(1e-4, 10, 100)
 
-    zalm_rates = [zalm_bell_fidelity_numerical(μ, ηR, ηT, Rp, N_I, N_M; engine=zalm_engine)
+    zalm_rates = [zalm_bell_fraction_numerical(μ, ηR, ηT; engine=zalm_engine)
                   for μ in μ_vals, ηT in ηT_vals]
-    chahine_rates = [chahine_bell_fidelity_numerical(μ, ηR, ηT, Rp, N_I, N_M; engine=chahine_engine)
+    chahine_rates = [chahine_bell_fraction_numerical(μ, ηR, ηT; engine=chahine_engine)
                      for μ in μ_vals, ηT in ηT_vals]
     # spdc_rates = [spdc_rate_dist_entanglement_numerical(μ, ηR, Rp, N_M; engine=spdc_engine) for μ in μ_vals, ηT in ηT_vals]
     #spcd_rates_symbolic = [spdc_rate_dist_entanglement_symbolic(μ, ηR, Rp, N_M) for μ in μ_vals, ηT in ηT_vals]
-
     #pbp4_rates = [pbp4_rate_dist_entanglement(μ, ηR, ηT, Rp, N_I, N_M; engine=pbp4_engine) for μ in μ_vals, ηT in ηT_vals]
     zmax = maximum(filter(!isnan, vcat(vec(zalm_rates), vec(chahine_rates))))
+    
+   
+    function source_roots(ηT)
+        1 / ηT
+    end
+    z_vals = logrange(1e-4, zmax, 50)  
+
+    μ_curtain_matrix  = [source_roots(ηT) for _ in z_vals, ηT in ηT_vals]
+    ηT_curtain_matrix = [ηT               for _ in z_vals, ηT in ηT_vals]
+    z_curtain_matrix  = [z                for z  in z_vals, _  in ηT_vals]
 
     fig = Figure(size = (1000, 700))
 
@@ -506,15 +602,19 @@ function plot_bell_fraction_with_Nm_3D()
     surfaces = [
         (name = "ZALM",    data = zalm_rates,    color = RGBAf(0.18, 0.545, 0.341, 0.8)),
         (name = "Chahine", data = chahine_rates, color = RGBAf(1.0,  0.271, 0.0,   0.8)),
-       #(name = "PBP4", data  = pbp4_rates, color = RGBAf(0.85, 0.74,  0.0,   0.4) )
+        #(name = "PBP4", data  = pbp4_rates, color = RGBAf(0.85, 0.74,  0.0,   0.4) )
         # (name = "SPDC",    data = spdc_rates,    color = RGBAf(0.85, 0.74,  0.0,   0.4)),
-        (name = "SPDC symbolic", data = spcd_rates_symbolic,  color = RGBAf(1.0,  0.271, 0.0,   0.8))
+        # (name = "SPDC symbolic", data = spcd_rates_symbolic,  color = RGBAf(1.0,  0.271, 0.0,   0.8)),
     ]
 
     for s in surfaces
         surface!(ax, μ_vals, ηT_vals, s.data;
             color = fill(s.color, size(s.data)), shading = NoShading)
     end
+    
+    curtain_color = RGBAf(0.3, 0.5, 0.9, 0.5)
+    surface!(ax, μ_curtain_matrix, ηT_curtain_matrix, z_curtain_matrix;
+        color = fill(curtain_color, size(z_curtain_matrix)), shading = NoShading)
 
     Legend(fig[2, 1],
         [PolyElement(color = s.color) for s in surfaces],
@@ -528,8 +628,10 @@ end
 try
     fig1 = plot_rate_dist_entanglement_with_Nm_3D()
     fig2 = plot_fidelity_with_Nm_3D()
-    display(fig1)
-    display(fig2)
+    fig3 = plot_bell_fraction_with_Nm_3D()
+    display(GLMakie.Screen(), fig1)
+    display(GLMakie.Screen(), fig2)
+    display(GLMakie.Screen(), fig3)
 catch e
     showerror(stdout, e, catch_backtrace())
 end
